e5825a047af4932bfcc882d87721d4686310a6b5
Aiso Dee <mad-homelab@outlook.com>
2026-01-25 21:08:53 +1100

    Deploy storage account for shared state file

--- services/observability-core/main.tf
+++ services/observability-core/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_log_analytics_workspace" "law_create" {
   name                = "${var.service_name}-law-${var.environment}"
   location            = module.rg_create.rg_location
-  resource_group_name = module.rg_create.rg_name
+  resource_group_name = module.rg_create.resource_group_name
   sku                 = "PerGB2018"
   retention_in_days   = 30
   daily_quota_gb      = 1
