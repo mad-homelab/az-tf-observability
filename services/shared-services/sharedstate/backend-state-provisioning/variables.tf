ae855b11fe8909fb1c745780ce937901c82bbd8b
Aiso Dee <mad-homelab@outlook.com>
2026-02-03 18:33:55 +1100

    Added app reg and sp creation for teams

---
+++ services/shared-services/sharedstate/backend-state-provisioning/variables.tf
@@ -0,0 +1,14 @@
+variable service_teams {
+    description = "Map of service teams to be provisioned with their own state and identity."
+    type = map(object({
+        display_name = string
+        repo_path = string
+    }))
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Environment resources are being deployed to."
+
+}
