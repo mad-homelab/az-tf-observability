088df1f66e1d4cd0fd33978ee07be83bbbb324ee
Aiso Dee <mad-homelab@outlook.com>
2026-02-04 12:51:43 +1100

    Changed container naming to show team name.

--- services/shared-services/sharedstate/backend-state-provisioning/main.tf
+++ services/shared-services/sharedstate/backend-state-provisioning/main.tf
@@ -43,7 +43,7 @@ module "service_team_storage_accts" {
   storage_account_name = lower(substr("madhl${each.value.short_name}${var.environment}", 0, 24))
   resource_group_name = module.service_team_rgs[each.key].resource_group_name
   resource_location = module.service_team_rgs[each.key].rg_location
-  container_name = "tfstate-${var.environment}"
+  container_name = "${each.value.short_name}-tfstate-${var.environment}"
 }
 
 # Assign RBAC RG Contributor
