3cc3429960bc471a18ecb121c22e9ceb4d5c2aa2
ma-dee <maaisobelle.au@gmail.com>
2026-02-19 12:13:22 +1100

    Refactored azpolicy naming convention (#52)
    Co-authored-by: Aiso Dee <mad-homelab@outlook.com>

--- services/shared-services/az-policies/outputs.tf
+++ services/shared-services/az-policies/outputs.tf
@@ -2,7 +2,7 @@ output "Policy_Manifest" {
   description = "A report of all deployed governance policies pulled directly from state."
   value = {
     for key, config in module.policy_assignments : key => {
-      
+      resource_type_key = key
       # Info from module deployments
       policy_name      = config.policy_name
       display_name     = config.display_name
@@ -13,7 +13,6 @@ output "Policy_Manifest" {
       # Info from the remediation resource
       remediation_name = try(azurerm_subscription_policy_remediation.remediations[key].name, "N/A (Audit Only)")
       
-      resource_type_tag = var.policy_configs[key].resource_type
       assigned_roles    = keys(var.policy_configs[key].roles)
     }
   }
